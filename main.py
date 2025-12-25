a62b9e1f317edb9ca93b4a95f0209ab84f7def47
RonaldPasion <52368740+RonaldPasion@users.noreply.github.com>
2025-12-25 09:58:32 -0800

    add collision between player and asteroid

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import sys
+
 import pygame
 
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_event, log_state
 from player import Player
 
 
@@ -30,10 +32,19 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         updatable.update(dt)
+
         for i in drawable:
             i.draw(screen)
+
+        for i in asteroids:
+            if i.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
